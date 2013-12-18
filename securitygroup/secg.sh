0a899f1458f4d6f3ef725c5d2a54a59d16a37a4d
IMURA Takashi <imura@realworld.jp>
2013-12-18 19:53:30 +0900

    modfiy secg.sh

--- securitygroup/secg.sh
+++ securitygroup/secg.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+#
+# curl -L https://raw.github.com/imura81gt/aws-tools/master/securitygroup/secg.sh | VPC_ID=vpc-xxxxxxxx SECG_NAME=hogehoge SECG_DESC='secg for hogehoge' IP_PERM_JSON=[https://example.com/hogehoge.json|file:///home/hogehoge/ipermissions.json] bash
+#
 
-SCRIPT_DIR=$(cd $(dirname $0) && pwd)
-echo $SCRIPT_DIR
-
-SECG_NAME="opp-batch"
-SECG_DESC="sg for opp batch"
-VPC_ID="vpc-6fb68607"
+#SECG_NAME="opp-batch"
+#SECG_DESC="sg for opp batch"
+#VPC_ID="vpc-6fb68607"
+#IP_PERM_JSON="file://${SCRIPT_DIR}/ippermissions.json"
 
 if [ $(aws ec2 describe-security-groups --filters Name=group-name,Values=$SECG_NAME | jq '.SecurityGroups | length') == 1 ]
 then
@@ -18,6 +19,7 @@ seg_g_result_array=(`aws ec2 create-security-group --group-name ${SECG_NAME} --d
 
 SECG_RETURN=${seg_g_result_array[0]}
 SECG_ID=${seg_g_result_array[1]}
+
 if [ "${SECG_RETURN}" = "false" ]
 then
  echo "Error: aws ec2 --group-name ${SECG_NAME} --description "${SECG_DESC}" --vpc-id ${VPC_ID}"
@@ -25,10 +27,11 @@ then
 fi
 
 echo "# Add rules"
-aws ec2 authorize-security-group-ingress --group-id ${SECG_ID} --ip-permissions "file://${SCRIPT_DIR}/ippermissions.json"
+aws ec2 authorize-security-group-ingress --group-id ${SECG_ID} --ip-permissions ${IP_PERM_JSON}
 
 echo "# The Security Group"
 aws ec2 describe-security-groups --filters Name=group-name,Values=${SECG_NAME} --query "SecurityGroups[0]"
 
 echo "# You can remove the Security Group by the following command."
 echo "aws ec2 delete-security-group --group-id ${SECG_ID}"
+
